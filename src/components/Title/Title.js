c20cd9b4cf57668436bb546f12653648b8d5472e
Dan Klinga <dklinga@Dans-MBP.fios-router.home>
2018-06-17 18:06:38 -0400

    Sunday

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+
+<div className="titlebox">
+<h1>Friend Clicky Game!</h1>
+<p>Click on an image to score</p>
+</div>
+);
+
+export default Title;
